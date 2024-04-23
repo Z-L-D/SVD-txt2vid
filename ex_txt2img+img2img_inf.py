a4616178c9eb843bfe7d9b4f11c2449ee4a0f273
Z-L-D <zacldoyle@gmail.com>
2024-04-23 08:14:27 -0500

    -

--- ex_txt2img+img2img_inf.py
+++
@@ -1,9 +0,0 @@
-import torch
-from diffusers import StableCascadeCombinedPipeline
-
-pipe = StableCascadeCombinedPipeline.from_pretrained(
-    "stabilityai/stable-cascade", variant="bf16", torch_dtype=torch.bfloat16
-)
-pipe.enable_model_cpu_offload()
-prompt = "an image of a shiba inu, donning a spacesuit and helmet"
-images = pipe(prompt=prompt)
